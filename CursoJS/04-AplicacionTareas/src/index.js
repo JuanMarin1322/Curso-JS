b69d1cdee34a70dcc0d933b0c1ef630d61edb9ac
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-06-02 22:13:08 -0500

    TodoList

--- CursoJS/04-AplicacionTareas/src/index.js
+++ CursoJS/04-AplicacionTareas/src/index.js
@@ -1,7 +1,16 @@
 import '../src/styles.css';
-import { Todo } from '../classes/todo.class.js';
 
+import { Todo, TodoList} from '../classes/index.js';
+import { crearTodoHtml } from './js/componentes.js';
+
+
+const todoList = new TodoList();
 
 const tarea  = new Todo('Aprender Javascript');
+const tarea2  = new Todo('Comprar un unicornio');
 
-console.log();
+todoList.nuevoTodo(tarea);
+todoList.nuevoTodo(tarea2);
+console.log(todoList);
+
+crearTodoHtml(tarea);
--- CursoJS/04-AplicacionTareas/src/js/componentes.js
+++ CursoJS/04-AplicacionTareas/src/js/componentes.js
@@ -1,19 +1,22 @@
-import '../css/componentes.css';
-// import  webpacklogo from '../assets/img/webpack-logo.png';
+
+export const crearTodoHtml = ( todo ) => {
 
 
-//Palabra clave export para loq eu se necesita exportar 
-export const saludar = ( nombre)=>{
-    console.log('Creando eriqueta h1')
+    const divTodoList = document.querySelector('.todo-list');
 
-    const h1 = document.createElement('h1');
-    h1.innerText=` !! Hola, ${nombre} !!!`;
+const htmlTodo =`<li class="${ (todo.completado )} ? 'completed'  : ''" data-id=" ${todo.id}">
+						<div class="view">
+							<input class="toggle" type="checkbox" ${ (todo.completado )} ? 'checked'  : ''>
+							<label>${todo.tarea}</label>
+							<button class="destroy"></button>
+						</div>
+						<input class="edit" value="Create a TodoMVC template">
+					</li>`;
+    const div = document.createElement('div');
 
-    document.body.append( h1 );
+    div.innerHTML= htmlTodo;
 
-    //IMG
-    
-    // const img = document.createElement('img');
-    // img.src = webpacklogo;
-    // document.body.append(img);
+    divTodoList.append( div.firstElementChild);
+
+    return div;
 }
