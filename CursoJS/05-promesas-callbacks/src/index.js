e52dd5c1e99fe2f1e04bd66fc4916f9c0e95d5d2
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-06-28 12:07:30 -0500

    Sesion 11

---
+++ CursoJS/05-promesas-callbacks/src/index.js
@@ -0,0 +1,21 @@
+import { buscarHeroe } from './js/callbacks'; 
+import './styles.css';
+
+
+console.log('Hola Mundo');
+
+
+const heroeId1 = 'capi';
+const heroeId2 = 'spider';
+
+buscarHeroe( heroeId1, ( error, heroe ) => {
+
+    // console.log('Callback llamado!');
+    if ( error){return console.error( error );} 
+    
+  
+        console.log(`Enviando a ${heroe.nombre} y ${heroe2.nombre} a la misión `);
+    
+    });
+
+});
