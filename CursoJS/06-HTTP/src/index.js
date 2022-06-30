0b2e3d0315f5152396e3aafc13a7798d270413d5
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-06-30 00:29:24 -0500

    Session 12

--- CursoJS/06-HTTP/src/index.js
+++ CursoJS/06-HTTP/src/index.js
@@ -1,16 +1,18 @@
-const { resolvePlugin } = require("@babel/core");
-const { response } = require("express");
-
-const jokeuRL = 'https://api.chucknorris.io/jokes/random';
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
 
-// fetch(jokeuRL).then( res => {
-    
-//     res.json().then( ({id, value}) => {
+// fetch (jokeUrl).then( res => {
+
+//     res.json().then( ({ id, value}) => {
 //         console.log(id);
 //         console.log(value);
 //     });
 // });
 
-fetch(jokeuRL).then(res => res.json() )
-.then( console.log)
+
+
+fetch (jokeUrl)
+.then( res => res.json())
+.then(({ id, value}) => {
+    console.log(id, value);
+});
