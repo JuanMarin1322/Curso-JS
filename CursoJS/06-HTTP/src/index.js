c884cbb45f8a73317aeda1762c266d4babc0de0f
JuanMarin1322 <91574074+JuanMarin1322@users.noreply.github.com>
2022-07-01 20:53:36 -0500

    Session 12

--- CursoJS/06-HTTP/src/index.js
+++ CursoJS/06-HTTP/src/index.js
@@ -1,18 +1,18 @@
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
 
-// fetch (jokeUrl).then( res => {
+// // fetch (jokeUrl).then( res => {
 
-//     res.json().then( ({ id, value}) => {
-//         console.log(id);
-//         console.log(value);
-//     });
+// //     res.json().then( ({ id, value}) => {
+// //         console.log(id);
+// //         console.log(value);
+// //     });
+// // });
+
+
+
+// fetch (jokeUrl)
+// .then( res => res.json())
+// .then(({ id, value}) => {
+//     console.log(id, value);
 // });
-
-
-
-fetch (jokeUrl)
-.then( res => res.json())
-.then(({ id, value}) => {
-    console.log(id, value);
-});
---
+++ CursoJS/06-HTTP/src/js/http-provider.js
@@ -0,0 +1,16 @@
+
+const jokeUrl = 'https://api.chucknorris.io/jokes/random';
+
+const obtenerChiste = async() => {
+
+      const resp =  await fetch(jokeUrl);
+
+};
+
+
+
+export {
+    
+    obtenerChiste
+
+};
